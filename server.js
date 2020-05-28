4cc21bfdf48a7c9233d00b2b5f8106316133858b
Junsu <murane@naver.com>
2020-05-28 21:46:40 +0900

    add cors on server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.port || 5000;
 
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
