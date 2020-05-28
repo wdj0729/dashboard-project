b42ae80af3df76c0724bca4daeb491869f6f329a
Junsu <murane@naver.com>
2020-05-29 03:58:23 +0900

    서울시 인실 점유율, 구간별 월세, 보증금 쿼리 API 추가

--- server.js
+++ server.js
@@ -22,24 +22,6 @@ const connection = mysql.createConnection({
 });
 connection.connect();
 
-app.get('/seoul',(req, res)=>{
-    connection.query(
-        "SELECT * FROM ROOMS",
-        (err, rows, fields) => {
-            res.send(rows);
-        }
-    )
-})
-
-app.get('/houses',(req, res)=>{
-    connection.query(
-        "SELECT * FROM ROOMS",
-        (err, rows, fields) => {
-            res.send(rows);
-        }
-    )
-})
-
 //서울시 매물 유형
 app.get('/api/seoul/house_type_distribution',(req, res)=>{
     connection.query(
@@ -54,5 +36,55 @@ app.get('/api/seoul/house_type_distribution',(req, res)=>{
         }
     )
 })
+
+//서울시 인실 점유율
+app.get('/api/seoul/bed_cnt_group',(req, res)=>{
+    connection.query(
+        `
+        SELECT r.bed_cnt, COUNT(r.bed_cnt) 갯수
+        FROM sharehouse.rooms as r
+        GROUP BY r.bed_cnt;
+        `,
+        (err, rows, fields) => {
+            res.send(rows);
+        }
+    )
+})
+
+//서울시 구간별 월세
+app.get('/api/seoul/bed_cnt_group',(req, res)=>{
+    connection.query(
+        `
+        SELECT count(if(b.monthly_rent>0 and b.monthly_rent <=20,true,null)) "0~20", 
+        count(if(b.monthly_rent>20 and b.monthly_rent <=40,true,null)) "20~40",
+        count(if(b.monthly_rent>40 and b.monthly_rent <=60,true,null)) "40~60",
+        count(if(b.monthly_rent>60 and b.monthly_rent <=80,true,null)) "60~80",
+        count(if(b.monthly_rent>80 and b.monthly_rent <=100,true,null)) "80~100",
+        count(if(b.monthly_rent>100,true,null)) "100~"
+        FROM sharehouse.beds as b;
+        `,
+        (err, rows, fields) => {
+            res.send(rows);
+        }
+    )
+})
+
+//서울시 구간별 보증금
+app.get('/api/seoul/bed_cnt_group',(req, res)=>{
+    connection.query(
+        `
+        SELECT count(if(b.deposit>0 and b.deposit <=300,true,null)) "0~300", 
+        count(if(b.deposit>300 and b.deposit <=500,true,null)) "300~500",
+        count(if(b.deposit>500 and b.deposit <=1000,true,null)) "500~1000",
+        count(if(b.deposit>1000,true,null)) "1000~"
+        FROM sharehouse.beds as b;
+        `,
+        (err, rows, fields) => {
+            res.send(rows);
+        }
+    )
+})
+
+
 
 app.listen(port, ()=> console.log(`Listening on port ${port}`));
